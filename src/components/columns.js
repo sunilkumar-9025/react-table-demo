1863905cd4939a52f2b31b95a35b1d6c9de2c34d
Sunil Kumar <sunil.kumar@tectoro.com>
2023-02-14 11:57:25 +0530

    1st

---
+++ src/components/columns.js
@@ -0,0 +1,77 @@
+export const COLUMNS = [
+  {
+    Header: "Id",
+    accessor: "id",
+    Footer: "Id",
+  },
+  {
+    Header: "First Name",
+    accessor: "first_name",
+    Footer: "First Name",
+  },
+  {
+    Header: "Last Name",
+    accessor: "last_name",
+    Footer: "Last Name",
+  },
+  {
+    Header: "Date of Birth",
+    accessor: "date_of_birth",
+    Footer: "Date of Birth",
+  },
+  {
+    Header: "Country",
+    accessor: "country",
+    Footer: "Country",
+  },
+  {
+    Header: "Phone",
+    accessor: "phone",
+    Footer: "Phone",
+  },
+];
+
+export const GROUPED_COLUMNS = [
+  {
+    Header: "Id",
+    accessor: "id",
+    Footer: "Id",
+  },
+  {
+    Header: "Name",
+    Footer: "Name",
+    columns: [
+      {
+        Header: "First Name",
+        accessor: "first_name",
+        Footer: "First Name",
+      },
+      {
+        Header: "Last Name",
+        accessor: "last_name",
+        Footer: "Last Name",
+      },
+    ],
+  },
+  {
+    Header: "Info",
+    Footer: "Info",
+    columns: [
+      {
+        Header: "Date of Birth",
+        accessor: "date_of_birth",
+        Footer: "Date of Birth",
+      },
+      {
+        Header: "Country",
+        accessor: "country",
+        Footer: "Country",
+      },
+      {
+        Header: "Phone",
+        accessor: "phone",
+        Footer: "Phone",
+      },
+    ],
+  },
+];
