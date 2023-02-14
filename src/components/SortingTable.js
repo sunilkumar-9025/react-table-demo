22d4c4d1f9290d6f9f9ba8ad40b4031bfd668851
Sunil Kumar <sunil.kumar@tectoro.com>
2023-02-14 12:28:44 +0530

    2nd

---
+++ src/components/SortingTable.js
@@ -0,0 +1,59 @@
+import React, { useMemo } from "react";
+import { useTable, useSortBy } from "react-table";
+import { COLUMNS } from "./columns";
+import MOCK_DATA from "./MOCK_DATA.json";
+import "./table.css";
+
+const SortingTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
+      },
+      useSortBy
+    );
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                {column.render("Header")}
+                <span>
+                  {column.isSorted ? (
+                    column.isSortedDesc ? (
+                      <img src="a.png" width={15} />
+                    ) : (
+                      <img src="b.png" width={15} />
+                    )
+                  ) : (
+                    ""
+                  )}
+                </span>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default SortingTable;
--- src/components/columns.js
+++ src/components/columns.js
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+
 export const COLUMNS = [
   {
     Header: "Id",
@@ -18,6 +20,9 @@ export const COLUMNS = [
     Header: "Date of Birth",
     accessor: "date_of_birth",
     Footer: "Date of Birth",
+    Cell: ({ value }) => {
+      return format(new Date(value), "yyyy MMMM dd");
+    },
   },
   {
     Header: "Country",
